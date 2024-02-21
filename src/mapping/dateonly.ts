bde5f655a8718cc1251cd2fac09e67d8d854ec06
Emile <emilemetry@outlook.com>
2024-02-21 10:29:43 -0400

    - removed unnecessary console.log()

--- src/mapping/dateonly.ts
+++ src/mapping/dateonly.ts
@@ -1,5 +1,4 @@
 export default function Map(date: string): Date | undefined {
     if(date === undefined || date.length === 0) return;
-    console.log(date);
     return new Date(date.split('T')[0] + 'T00:00:00.000');
 }
